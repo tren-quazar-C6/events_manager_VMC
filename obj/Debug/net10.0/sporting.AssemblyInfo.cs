50a1eb3f72d83b1ae41f928bbe97ba8872867170
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-04-17 20:53:35 -0500

    local version

--- obj/Debug/net10.0/sporting.AssemblyInfo.cs
+++ obj/Debug/net10.0/sporting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sporting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cfa8b4d2652eb82f16b5c62131fb9e256f2f3d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82608e4bd12e844d947f7a22f56767740c6c8ca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("sporting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sporting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
