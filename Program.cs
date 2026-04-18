6ab64fdae5f5ba928ef6d19b142a393e69c6eb36
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-04-18 11:22:17 -0500

    styles: emojis replaced

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
         name: "default",
-        pattern: "{controller=User}/{action=Index}/{id?}")
+        pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
